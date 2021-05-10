419a8e48adb2f6bcb266c6eb2b927788973feefd
Your Name <you@example.com>
2021-05-10 20:34:34 +0530

    bar-chart-complete

--- src/App.js
+++ src/App.js
@@ -4,13 +4,18 @@ import Chart from './Chart'
 
 const App = () => {
 
-  const datas = [12,36,55,25,35,10,40,30,45, 22]
-  const w = 700
-  const h = 500
+  const datas = [
+    { id: '1', region: 'China', value: 14 },
+    { id: '2', region: 'India', value: 10 },
+    { id: '3', region: 'USA', value: 16 },
+    { id: '4', region: 'Ghana', value: 8 },
+    { id: '5', region: 'Phillipines', value: 11 },
+    { id: '6', region: 'Germany', value: 12 },
+  ]
 
   return (
     <div>
-      <Chart datas={datas} w={w} h={h} />
+      <Chart datas={datas} />
     </div>
   )
 }
--- src/Chart.js
+++ src/Chart.js
@@ -1,36 +1,69 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
 import * as d3 from 'd3'
 
-const Chart = ({ datas, w, h }) => {
+const Chart = ({ datas }) => {
 
-    const myRef = useRef()
+    const CHART_WIDTH = 800
+    const CHART_HEIGHT = 400
+
+    const xScale = d3.scaleBand().rangeRound([0 ,CHART_WIDTH]).padding(0.2).domain(datas.map(d => d.region))
+    const yScale = d3.scaleLinear().range([CHART_HEIGHT, 0]).domain([0, d3.max(datas, d => d.value) + 11])
 
     useEffect(() => {
 
-       const accessToRef = d3.select(myRef.current)
-        .append("svg")
-        .attr("width",w)
-        .attr("height",h)
-        .style("background-color", "#cccccc")
-        .style("padding", 10)
-        .style("margin-left", 50);
+        const chartContainer = d3
+        .select('svg')
+        .attr('width', CHART_WIDTH)
+        .attr('height', CHART_HEIGHT + 20)
 
-        accessToRef.selectAll("rect")
-            .data(datas)
-            .enter()
-            .append("rect")
-            .attr("x", (d,i) => i *70)
-            .attr("y", (d,i) => h-10*d)
-            .attr("width", 65)
-            .attr("height", (d,i) => d*10)
-            .attr("fill", (d,i) => d > 35 ? "tomato" : "green")
+        const chart = chartContainer
+
+        chart
+        .append('g')
+        .call(d3.axisBottom(xScale).tickSizeOuter(0))
+        .attr('transform', `translate(0, ${CHART_HEIGHT})`)
+
+        chart
+        .append('g')
+        .call(d3.axisLeft(yScale).tickSizeOuter(0))
+        .attr('transform', `translate(20, 0)`)
+
+        chart
+        .selectAll('rect')
+        .data(datas)
+        .enter()
+        .append('rect')
+        .classed('bar', true)
+        .attr('width', xScale.bandwidth()) 
+        .attr('height', data => CHART_HEIGHT - yScale(data.value))
+        .attr('x', data => xScale(data.region)) 
+        .attr('y', data => yScale(data.value))
+
+        chart
+        .selectAll('p')
+        .data(datas)
+        .enter()
+        .append('text')
+        .text(data => data.value)
+        .attr('x', data => xScale(data.region) + xScale.bandwidth() / 2)
+        .attr('y', data => yScale(data.value) - 10) 
+        .attr('text-anchor', 'middle')
+
     }, [])
 
     return (
-        <div className="container">
-            <div ref={myRef}></div>
-        </div>
+        <div id="app">
+            <div id="chart">
+                <p style={{textAlign: 'center'}}>No. Of Trillion In Dollars($)</p>
+                <svg></svg>
+                <p style={{textAlign: 'center'}}>Countries</p>
+            </div>
+        </div>      
     )
 }
 
 export default Chart
+
+
+
+
